ee5637d4929080f36a5184574bbf513c044e3b74
No lifer <davydqq@gmail.com>
2023-02-04 21:41:22 +0200

    init project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Akka.Actor;
+
+var system = ActorSystem.Create("MySystem");
+var greeter = system.ActorOf<GreetingActor>("greeter");
+greeter.Tell(new Greet("Hello World"));
+
+Console.Read();
+
+
+public class Greet
+{
+    public Greet(string who)
+    {
+        Who = who;
+    }
+    public string Who { get; private set; }
+}
+
+public class GreetingActor : ReceiveActor
+{
+    public GreetingActor()
+    {
+        Receive<Greet>(greet =>
+        {
+            Console.WriteLine("[Thread {0}] Greeting {1}", Thread.CurrentThread.ManagedThreadId, greet.Who);
+        });
+    }
+}
